a51fb82ca795e8bf75a8d6fa60217e5b94bc1b26
Asif Manan <asifmanan@gmail.com>
2024-02-25 14:45:25 +0000

    Main method created to test the code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        CustomerCreditLimit customer = new CustomerCreditLimit();
+        System.out.println(customer.getCustomerName());
+        System.out.println(customer.getEmail());
+        System.out.println(customer.getCreditLimit());
+        System.out.println("-".repeat(30));
+        CustomerCreditLimit customer2 = new CustomerCreditLimit("Asif M.", "asifm@email.com");
+        System.out.println(customer2.getCustomerName());
+        System.out.println(customer2.getEmail());
+        System.out.println(customer2.getCreditLimit());
+        System.out.println("-".repeat(30));
+        CustomerCreditLimit customer3 = new CustomerCreditLimit("Asif M.", 50000d, "asifm@email.com");
+        System.out.println(customer3.getCustomerName());
+        System.out.println(customer3.getEmail());
+        System.out.println(customer3.getCreditLimit());
+    }
 }
