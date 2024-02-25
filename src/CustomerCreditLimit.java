9f3194a99ad7e2f971db115618fc485b2d8999f2
Asif Manan <asifmanan@gmail.com>
2024-02-25 14:44:56 +0000

    CustomerCreditLimit Class Created with 3 Constructors using contructor chaining, setter methods also defined

--- src/CustomerCreditLimit.java
+++ src/CustomerCreditLimit.java
@@ -1,2 +1,30 @@
 public class CustomerCreditLimit {
+    private String customerName;
+    private double creditLimit;
+    private String email;
+
+    public CustomerCreditLimit(){
+        this("Unknown", "");
+    }
+
+    public CustomerCreditLimit(String customerName, String email){
+        this("Unknown", 0.00,email);
+    }
+    public CustomerCreditLimit(String customerName, double creditLimit, String email) {
+        this.customerName = customerName;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
